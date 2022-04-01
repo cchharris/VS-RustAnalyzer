7f6debee5109748b59d40743ef2c774bc4dc3676
cchharris <cchharris@outlook.com>
2022-03-31 20:31:19 -0700

    Initial Commit

---
+++ VS-RustAnalyzer/RustLanguageClient.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Threading;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VS_RustAnalyzer
+{
+    [ContentType("rust")]
+    [Export(typeof(ILanguageClient))]
+    internal class RustLanguageClient : ILanguageClient
+    {
+        public string Name => "Rust Language Extension";
+
+        public IEnumerable<string> ConfigurationSections => null;
+
+        public object InitializationOptions => null;
+
+        public IEnumerable<string> FilesToWatch => null;
+
+        public event AsyncEventHandler<EventArgs> StartAsync;
+        public event AsyncEventHandler<EventArgs> StopAsync;
+
+        // TODO: Configure
+        public string ExecutablePath => @"C:\Users\cchha\.rust-analyzer\rust-analyzer-x86_64-pc-windows-msvc\rust-analyzer.exe";
+
+        public bool ShowNotificationOnInitializeFailed => true;
+
+        public async Task<Connection> ActivateAsync(CancellationToken token)
+        {
+            await Task.Yield();
+
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = ExecutablePath;
+            info.RedirectStandardInput = true;
+            info.RedirectStandardOutput = true;
+            info.UseShellExecute = false;
+            info.CreateNoWindow = true;
+
+            Process process = new Process();
+            process.StartInfo = info;
+
+            if (process.Start())
+            {
+                return new Connection(process.StandardOutput.BaseStream, process.StandardInput.BaseStream);
+            }
+
+            return null;
+        }
+
+        public async Task OnLoadedAsync()
+        {
+            await StartAsync.InvokeAsync(this, EventArgs.Empty);
+        }
+
+        public Task OnServerInitializeFailedAsync(Exception e)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnServerInitializedAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<InitializationFailureContext> OnServerInitializeFailedAsync(ILanguageClientInitializationInfo initializationState)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
